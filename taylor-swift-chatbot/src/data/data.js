56810c9e317ca52aa6ec868777faef2b41851f93
Mariam01010010 <mariamraashid@hotmail.com>
2024-02-24 13:58:03 +0000

    Added test data to test chatbot functionality

---
+++ taylor-swift-chatbot/src/data/data.js
@@ -0,0 +1,32 @@
+export const articles = {
+  wiki_main: "https://en.wikipedia.org/wiki/Taylor_Swift",
+  wiki_discography:
+    "https://en.wikipedia.org/wiki/Taylor_Swift_albums_discography",
+  wiki_live_performances:
+    "https://en.wikipedia.org/wiki/List_of_Taylor_Swift_live_performances",
+  wiki_tours_eras_tour: "https://en.wikipedia.org/wiki/The_Eras_Tour",
+  wiki_album_one_taylor_swift:
+    "https://en.wikipedia.org/wiki/Taylor_Swift_(album)",
+  wiki_album_two_fearless:
+    "https://en.wikipedia.org/wiki/Fearless_(Taylor_Swift_album)",
+  wiki_album_three_speak_now: "https://en.wikipedia.org/wiki/Speak_Now",
+  wiki_album_four_red: "https://en.wikipedia.org/wiki/Red_(Taylor_Swift_album)",
+  wiki_album_five_1989: "https://en.wikipedia.org/wiki/1989_(album)",
+  wiki_album_six_reputation: "https://en.wikipedia.org/wiki/Reputation_(album)",
+  wiki_album_seven_lover: "https://en.wikipedia.org/wiki/Lover_(album)",
+  wiki_album_eight_folklore:
+    "https://en.wikipedia.org/wiki/Folklore_(Taylor_Swift_album)",
+  wiki_album_nine_evermore:
+    "https://en.wikipedia.org/wiki/Evermore_(Taylor_Swift_album)",
+  wiki_album_ten_midnights: "https://en.wikipedia.org/wiki/Midnights",
+  wiki_album_eleven_the_tortured_poets_departments:
+    "https://en.wikipedia.org/wiki/The_Tortured_Poets_Department",
+  wiki_album_two_rerecorded_fearless:
+    "https://en.wikipedia.org/wiki/Fearless_(Taylor%27s_Version)",
+  wiki_album_three_rerecorded_speak_now:
+    "https://en.wikipedia.org/wiki/Speak_Now_(Taylor%27s_Version)",
+  wiki_album_four_rerecorded_red:
+    "https://en.wikipedia.org/wiki/Red_(Taylor%27s_Version)",
+  wiki_album_five_rerecorded_1989:
+    "https://en.wikipedia.org/wiki/1989_(Taylor%27s_Version)",
+};
